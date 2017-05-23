60fe9c2a60d8dc6d15b96dad5d85e7e880c171af
Alexey Berdnikov <aberdnikov@gmail.com>
2017-05-23 12:51:28 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by Larakit.
+ * Link: http://github.com/larakit
+ * User: Alexey Berdnikov
+ * Date: 23.05.17
+ * Time: 12:42
+ */
+Larakit\Boot::register_provider(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
